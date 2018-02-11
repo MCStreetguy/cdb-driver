6aefa5fe800f2a9b16070ed60799f7e8bd29e679
Maximilian Schmidt <m.schmidt@obis-concept.de>
2018-02-11 01:31:57 +0100

    Introducing automated tests

---
+++ tests/main.test.js
@@ -0,0 +1,42 @@
+const colors = require('colors');
+const CDBDriver = require('../app/main.js');
+
+console.log('Invoking main test script...'.underline.bold.red);
+
+// Config object, no auth
+console.log('Creating new CDBDriver using config object without authentication:'.yellow);
+var driver_object_noauth = new CDBDriver({
+  host: 'http://localhost/',
+  port: 5984
+});
+console.log(driver_object_noauth);
+console.log('');
+
+// Config object + auth
+console.log('Creating new CDBDriver using config object with authentication:'.yellow);
+var driver_object_auth = new CDBDriver({
+  host: 'http://localhost/',
+  port: 5984,
+  user: 'testuser',
+  pass: 'myn1c3p455'
+});
+console.log(driver_object_auth);
+console.log('');
+
+// Config string, no auth
+console.log('Creating new CDBDriver using config string without authentication:'.yellow);
+var driver_string_noauth = new CDBDriver(
+  'http://localhost:5984/'
+);
+console.log(driver_string_noauth);
+console.log('');
+
+// Config string + auth
+console.log('Creating new CDBDriver using config string with authentication:'.yellow);
+var driver_string_auth = new CDBDriver(
+  'http://testuser:myn1c3p455@localhost:5984/'
+);
+console.log(driver_string_auth);
+console.log('');
+
+console.log('Done testing.');
